b6f6d94321a8f0be74df80ba9e9690c56511248b
imritikmalhotra <57611324+imritikmalhotra@users.noreply.github.com>
2021-05-27 23:41:24 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,163 @@
+
+var a = document.getElementById("myAudioa"); 
+function Aplay() { 
+  a.play(); 
+} 
+
+function Apause() { 
+  a.pause(); 
+} 
+
+var b = document.getElementById("myAudiob"); 
+function Bplay() { 
+  b.play(); 
+} 
+
+function Bpause() { 
+  b.pause(); 
+} 
+
+var c = document.getElementById("myAudioc"); 
+function Bplay() { 
+  c.play(); 
+} 
+
+function Bpause() { 
+  c.pause(); 
+} 
+
+var d = document.getElementById("myAudiod"); 
+function dplay() { 
+  d.play(); 
+} 
+
+function dpause() { 
+  d.pause(); 
+} 
+
+var e = document.getElementById("myAudioe"); 
+function eplay() { 
+  e.play(); 
+} 
+
+function epause() { 
+  e.pause(); 
+} 
+
+var f = document.getElementById("myAudiof"); 
+function fplay() { 
+  f.play(); 
+} 
+
+function fpause() { 
+  f.pause(); 
+} 
+
+var g = document.getElementById("myAudiog"); 
+function gplay() { 
+  g.play(); 
+} 
+
+function gpause() { 
+  g.pause(); 
+} 
+
+
+var h = document.getElementById("myAudioh"); 
+function hplay() { 
+  h.play(); 
+} 
+
+function hpause() { 
+  h.pause(); 
+} 
+
+var i = document.getElementById("myAudioi"); 
+function iplay() { 
+  i.play(); 
+} 
+
+function ipause() { 
+  i.pause(); 
+} 
+
+var j = document.getElementById("myAudioj"); 
+function jplay() { 
+  j.play(); 
+} 
+
+function jpause() { 
+  j.pause(); 
+} 
+
+var k = document.getElementById("myAudiok"); 
+function kplay() { 
+  k.play(); 
+} 
+
+function kpause() { 
+  k.pause(); 
+} 
+
+var l = document.getElementById("myAudiol"); 
+function lplay() { 
+  l.play(); 
+} 
+
+function lpause() { 
+  l.pause(); 
+} 
+
+var m = document.getElementById("myAudiom"); 
+function mplay() { 
+  m.play(); 
+} 
+
+function mpause() { 
+  m.pause(); 
+} 
+
+var n = document.getElementById("myAudion"); 
+function nplay() { 
+  n.play(); 
+} 
+
+function npause() { 
+  n.pause(); 
+} 
+
+var o = document.getElementById("myAudioo"); 
+function oplay() { 
+  o.play(); 
+} 
+
+function opause() { 
+  o.pause(); 
+} 
+
+var p = document.getElementById("myAudiop"); 
+function pplay() { 
+  p.play(); 
+} 
+
+function ppause() { 
+  p.pause(); 
+} 
+
+var q = document.getElementById("myAudioq"); 
+function qplay() { 
+  q.play(); 
+} 
+
+function qpause() { 
+  q.pause(); 
+} 
+
+var r = document.getElementById("myAudior"); 
+function rplay() { 
+  r.play(); 
+} 
+
+function rpause() { 
+  r.pause(); 
+} 
